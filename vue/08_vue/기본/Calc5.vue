1c80ea7baf188a4235805522d0f0e7a9f8dda374
woojeong98 <wooo9806@gmail.com>
2025-04-07 01:07:40 +0900

    web full stack 실습과제

---
+++ vue/08_vue/기본/Calc5.vue
@@ -0,0 +1,29 @@
+<!-- 문제 (page 11): Calc5 컴포넌트를 다음처럼 정의하고, 필요한 경우 코드를 완성하세요. 
+o x의 값이 변결될 때마다,
+o 이전 값과 새로운 값을 브라우저 콘솔에 출력함.
+-->
+
+<template>
+    <div>
+        X: <input type="text" v-model.number="state.x"><br/>
+        <div>결과: {{ result }}</div>
+    </div>
+</template>
+
+<script>
+import { watch, ref } from 'vue'
+
+export default {
+    name: "Calc5",
+    setup(){
+        const x = ref(0);
+        const result = ref(0)
+
+        watch(x, (current, old) => {
+            console.log(`${old} ==> ${current}`)
+            result.value = current*2;
+        })
+        return {x, result}
+    }
+}
+</script>
